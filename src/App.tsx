b2b002bd66f1b45556e65f7c4178207c82344eb8
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-11 11:41:09 +0200

    website scheme /w routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
- 
-
   return (
     <>
-    <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
