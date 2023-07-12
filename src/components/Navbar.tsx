2eccd1b69c9966307611fb408ac8adeb6a22208a
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-12 19:22:56 +0200

    conditional navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import netflixLogo from "../assets/netflix-logo.png";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = (): JSX.Element => {
+  const { user, logOut } = UserAuth();
+
   return (
     <div className="w-full flex items-center justify-between py-6 px-12 z-[100] fixed">
-        <Link to="/">
+      <Link to="/">
         <img className="w-32" src={netflixLogo} />
-        </Link>
-      
-      <div>
-        <Link to="/login">
-        <button className="text-white pr-4 cursor-pointer">Sign In</button>
-        </Link>
-        <Link to="/signup">
-             <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
-          Sign Up
-        </button>
-        </Link>
-      </div>
+      </Link>
+
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4 cursor-pointer">Account</button>
+          </Link>
+          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4 cursor-pointer">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
