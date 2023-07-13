eed41c23ba1379d85a8c57117583c2844861f2ae
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-13 10:56:40 +0200

    logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,36 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import netflixLogo from "../assets/netflix-logo.png";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = (): JSX.Element => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-full flex items-center justify-between py-6 px-12 z-[100] fixed">
       <Link to="/">
         <img className="w-32" src={netflixLogo} />
       </Link>
-
       {user?.email ? (
         <div>
           <Link to="/account">
             <button className="text-white pr-4 cursor-pointer">Account</button>
           </Link>
-          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white"
+          >
             Logout
           </button>
         </div>
